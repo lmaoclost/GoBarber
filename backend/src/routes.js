cdc3efadc99ca1caf5b0385e0659897ff93d8ef8
lmaoclost <renansmoliveira@gmail.com>
2020-05-07 16:33:15 -0300

    Sequelize, User & Session Crud, Token and Encrypt

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,5 +1,14 @@
 import { Router } from 'express';
+import UserController from './app/controller/UserController';
+import SessionController from './app/controller/SessionController';
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+routes.put('/users', UserController.update);
 
 export default routes;
