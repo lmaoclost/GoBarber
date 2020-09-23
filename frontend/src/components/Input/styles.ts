09ae6dd3457e6aed37f87d10a0b6f7476bd06140
lmaoclost <renansmoliveira@gmail.com>
2020-09-23 17:28:15 -0300

    Isolating components

---
+++ frontend/src/components/Input/styles.ts
@@ -0,0 +1,37 @@
+import styled from 'styled-components';
+
+interface ContainerProps {
+  isFocused: boolean;
+  isFilled: boolean;
+  isErrored: boolean;
+}
+
+export const Container = styled.div`
+  background: #232129;
+  border-radius: 10px;
+  border: 2px solid #222129;
+  padding: 16px;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  color: #666360;
+
+  & + div {
+    margin-top: 8px;
+  }
+
+  input {
+    flex: 1;
+    background: transparent;
+    border: 0;
+    color: #f4ede8;
+
+    &::placeholder {
+      color: #666360;
+    }
+  }
+
+  svg {
+    margin-right: 16px;
+  }
+`;
