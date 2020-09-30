aa80ac8f00eb42468f85ed5a8bce0c87892271f0
lmaoclost <renansmoliveira@gmail.com>
2020-09-30 17:19:42 -0300

    ResetPasswordService Tests

--- backend/src/modules/users/repositories/fakes/FakeUserTokensRepository.ts
+++ backend/src/modules/users/repositories/fakes/FakeUserTokensRepository.ts
@@ -15,9 +15,19 @@ class UserTokensRepository implements IUserTokensRepository {
       id: uuid(),
       token: uuid(),
       user_id,
+      created_at: new Date(),
+      updated_at: new Date(),
     });
 
     this.userTokens.push(userToken);
+
+    return userToken;
+  }
+
+  public async findByToken(token: string): Promise<UserToken | undefined> {
+    const userToken = this.userTokens.find(
+      (findToken) => findToken.token === token
+    );
 
     return userToken;
   }
