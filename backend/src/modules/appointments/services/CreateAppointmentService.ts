4d04cc5b4c82299eb9f7fdd401cf8b388163e125
lmaoclost <renansmoliveira@gmail.com>
2020-10-02 16:29:05 -0300

    CreateAppointment new conditions

--- backend/src/modules/appointments/services/CreateAppointmentService.ts
+++ backend/src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,4 +1,4 @@
-import { startOfHour } from 'date-fns';
+import { startOfHour, isBefore, getHours } from 'date-fns';
 import { injectable, inject } from 'tsyringe';
 
 import AppError from '@shared//errors/AppError';
@@ -25,6 +25,18 @@ class CreateAppointmentService {
     user_id,
   }: IRequest): Promise<Appointment> {
     const appointmentDate = startOfHour(date);
+
+    if (isBefore(appointmentDate, Date.now())) {
+      throw new AppError('The appointment can not be at past');
+    }
+
+    if (user_id === provider_id) {
+      throw new AppError('You cannot create an appointment to yourself');
+    }
+
+    if (getHours(appointmentDate) < 8 || getHours(appointmentDate) > 17) {
+      throw new AppError('The appointment start needs to be between 8 and 17');
+    }
 
     const findAppointmentInSameDate = await this.appointmentsRepository.findByDate(
       appointmentDate
