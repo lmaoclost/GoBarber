4b10e26d9dd4bab187442e134b653682e39dbd60
lmaoclost <renansmoliveira@gmail.com>
2020-10-02 17:03:31 -0300

    Routes of Availability, InsomniaFile Update

--- backend/src/modules/appointments/infra/http/routes/providers.routes.ts
+++ backend/src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -1,13 +1,27 @@
 import { Router } from 'express';
 
 import ProvidersController from '@modules/appointments/infra/http/controllers/ProvidersController';
+import ProviderMonthAvailabilityController from '@modules/appointments/infra/http/controllers/ProviderMonthAvailabilityController';
+import ProviderDayAvailabilityController from '@modules/appointments/infra/http/controllers/ProviderDayAvailabilityController';
+
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 
 const providersRouter = Router();
 const providersController = new ProvidersController();
+const providerMonthAvailabilityController = new ProviderMonthAvailabilityController();
+const providerDayAvailabilityController = new ProviderDayAvailabilityController();
 
 providersRouter.use(ensureAuthenticated);
 
 providersRouter.get('/', providersController.index);
+
+providersRouter.get(
+  '/:provider_id/month-availability',
+  providerMonthAvailabilityController.index
+);
+providersRouter.get(
+  '/:provider_id/day-availability',
+  providerDayAvailabilityController.index
+);
 
 export default providersRouter;
