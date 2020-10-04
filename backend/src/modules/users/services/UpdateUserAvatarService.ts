4cf00cb88c40a50d851098097da524da6b518efc
lmaoclost <renansmoliveira@gmail.com>
2020-10-04 15:05:04 -0300

    S3 Storage

--- backend/src/modules/users/services/UpdateUserAvatarService.ts
+++ backend/src/modules/users/services/UpdateUserAvatarService.ts
@@ -1,30 +1,34 @@
 import path from 'path';
 import fs from 'fs';
-import { injectable, inject } from 'tsyringe';
 
 import uploadConfig from '@config/upload';
-import AppError from '@shared/errors/AppError';
 
 import User from '@modules/users/infra/typeorm/entities/User';
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import IStorageProvider from '@shared/container/providers/StorageProvider/models/IStorageProvider';
-import IUsersRepository from '../repositories/IUsersRepository';
 
-interface IRequest {
+import AppError from '@shared/errors/AppError';
+
+import { injectable, inject } from 'tsyringe';
+
+interface IRequestDTO {
   user_id: string;
   avatarFilename: string;
 }
-
 @injectable()
 class UpdateUserAvatarService {
   constructor(
     @inject('UsersRepository')
     private usersRepository: IUsersRepository,
 
-    @inject('StorageProvider')
-    private storageProvider: IStorageProvider
+    @inject('DiskStorageProvider')
+    private diskStorageProvider: IStorageProvider
   ) {}
 
-  public async execute({ user_id, avatarFilename }: IRequest): Promise<User> {
+  public async execute({
+    user_id,
+    avatarFilename,
+  }: IRequestDTO): Promise<User> {
     const user = await this.usersRepository.findById(user_id);
 
     if (!user) {
@@ -32,10 +36,10 @@ class UpdateUserAvatarService {
     }
 
     if (user.avatar) {
-      await this.storageProvider.deleteFile(user.avatar);
+      await this.diskStorageProvider.deleteFile(user.avatar);
     }
 
-    const filename = await this.storageProvider.saveFile(avatarFilename);
+    const filename = await this.diskStorageProvider.saveFile(avatarFilename);
 
     user.avatar = filename;
 
--- backend/src/shared/container/providers/StorageProvider/index.ts
+++ backend/src/shared/container/providers/StorageProvider/index.ts
@@ -1,12 +1,15 @@
 import { container } from 'tsyringe';
+import uploadConfig from '@config/upload';
 
 import DiskStorageProvider from '@shared/container/providers/StorageProvider/implementations/DiskStorageProvider';
+import S3DiskStorageProvider from '@shared/container/providers/StorageProvider/implementations/S3DiskStorageProvider';
 import IStorageProvider from './models/IStorageProvider';
 
 const providers = {
   disk: DiskStorageProvider,
+  s3: S3DiskStorageProvider,
 };
 container.registerSingleton<IStorageProvider>(
   'DiskStorageProvider',
-  providers.disk
+  providers[uploadConfig.driver]
 );
