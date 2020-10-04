cbacce559cc6299516275c1a490dc318ce5ed61e
lmaoclost <renansmoliveira@gmail.com>
2020-10-04 12:16:47 -0300

    AWS SES Configuration

---
+++ backend/src/config/mail.ts
@@ -0,0 +1,19 @@
+interface IMailConfig {
+  driver: 'ethereal' | 'ses';
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+  defaults: {
+    from: {
+      email: 'example@example.com',
+      name: 'Example GoStack',
+    },
+  },
+} as IMailConfig;
---
+++ backend/src/shared/container/providers/MailProvider/implementations/SESMailProvider.ts
@@ -0,0 +1,48 @@
+import nodemailer, { Transporter } from 'nodemailer';
+import { injectable, inject } from 'tsyringe';
+import aws from 'aws-sdk';
+import mailConfig from '@config/mail';
+
+import ISendMailDTO from '@shared/container/providers/MailProvider/dtos/ISendMailDTO';
+import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
+
+import IMailTemplateProvider from '@shared/container/providers/MailTemplateProvider/models/IMailTemplateProvider';
+
+@injectable()
+export default class SESMailProvider implements IMailProvider {
+  private client: Transporter;
+
+  constructor(
+    @inject('MailTemplateProvider')
+    private mailTemplateProvider: IMailTemplateProvider
+  ) {
+    this.client = nodemailer.createTransport({
+      SES: new aws.SES({
+        apiVersion: '2010-12-01',
+        region: process.env.AWS_DEFAULT_REGION,
+      }),
+    });
+  }
+
+  public async sendMail({
+    to,
+    from,
+    subject,
+    templateData,
+  }: ISendMailDTO): Promise<void> {
+    const { name, email } = mailConfig.defaults.from;
+
+    await this.client.sendMail({
+      to: {
+        name: to.name,
+        address: to.email,
+      },
+      from: {
+        name: from?.name || name,
+        address: from?.email || email,
+      },
+      subject,
+      html: await this.mailTemplateProvider.parse(templateData),
+    });
+  }
+}
