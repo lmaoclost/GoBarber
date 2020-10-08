8303a2bb6b2119e7edc720534884a90a282a8bf3
lmaoclost <renansmoliveira@gmail.com>
2020-10-08 12:26:23 -0300

    Dashboard Page styles and API

--- mobile/src/hooks/auth.tsx
+++ mobile/src/hooks/auth.tsx
@@ -45,7 +45,10 @@ const AuthProvider: React.FC = ({ children }) => {
         '@GoBarber:user',
       ]);
 
+      api.defaults.headers.authorization = `Bearer ${token}`;
+
       if (token[1] && user[1]) {
+        api.defaults.headers.authorization = `Bearer ${token[1]}`;
         setData({ token: token[1], user: JSON.parse(user[1]) });
       }
       setLoading(false);
